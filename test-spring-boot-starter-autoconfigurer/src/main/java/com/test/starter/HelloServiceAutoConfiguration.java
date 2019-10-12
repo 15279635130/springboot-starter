c4d7e61f050205c9d445a5d00912875eab756a16
yulinh <2504852383@qq.com>
2019-10-12 10:00:23 +0800

    SpringBoot custom starter

---
+++ test-spring-boot-starter-autoconfigurer/src/main/java/com/test/starter/HelloServiceAutoConfiguration.java
@@ -0,0 +1,23 @@
+package com.test.starter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnWebApplication    // web应用才生效
+@EnableConfigurationProperties(HelloProperties.class)   // 将HelloProperties注入IOC容器
+public class HelloServiceAutoConfiguration {
+
+    @Autowired
+    private HelloProperties helloProperties;
+
+    @Bean
+    public HelloService helloService() {
+        HelloService helloService = new HelloService();
+        helloService.setHelloProperties(helloProperties);
+        return helloService;
+    }
+}
